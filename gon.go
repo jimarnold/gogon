7b44cc3dacf7823aad6c37d25519b6b2463f641b
Jim Arnold <jim@thoughtworks.com>
2012-11-08 11:07:50 -0500

    cleaned up render settings; removed some magic numbers; fixed key handling

--- gon.go
+++ gon.go
@@ -8,22 +8,22 @@ import "github.com/go-gl/gl"
 import "github.com/go-gl/glfw"
 
 var things []Thing
+const width float64 = 800
+const height float64 = 600
 
 func main() {
   things = make([]Thing, 16)
   rand.Seed(time.Now().UTC().UnixNano())
   for i:=range things {
-    things[i].location = Vector{rand.Float64() * 800, rand.Float64() * 600}
-    things[i].direction = Vector{(0.5 - rand.Float64()) * 10, (0.5 - rand.Float64()) * 10}
+    things[i].location = Vector{random(0,1) * width, random(0,1) * height}
+    things[i].direction = Vector{random(-1,1), random(-1,1)}
     things[i].size = float64(i)
-    fmt.Printf("%d direction %f,%f", i, things[i].direction.x, things[i].direction.y)
   }
-  initGlfw(800,600)
+  initGlfw(int(width),int(height))
 
   for glfw.WindowParam(glfw.Opened) == 1 {
     elapsed := 0.01666666
     update(elapsed)
-    //draw(elapsed)
     render()
     glfw.SwapBuffers()
   }
@@ -32,21 +32,21 @@ func main() {
 func update(elapsed float64) {
   for i:= range things {
     thing := things[i]
-    newX := thing.location.x + (elapsed * thing.direction.x * 10)
-    newY := thing.location.y + (elapsed * thing.direction.y * 10)
+    newX := thing.location.x + (elapsed * thing.direction.x * 100)
+    newY := thing.location.y + (elapsed * thing.direction.y * 100)
     thing.location.x = newX
     thing.location.y = newY
-    if thing.location.x > 800 {
-      thing.location.x = thing.location.x - 800
+    if thing.location.x > width {
+      thing.location.x = thing.location.x - width
     }
     if thing.location.x < 0 {
-      thing.location.x = thing.location.x + 800
+      thing.location.x = thing.location.x + width
     }
-    if thing.location.y > 600 {
-      thing.location.y = thing.location.y - 600
+    if thing.location.y > height {
+      thing.location.y = thing.location.y - height
     }
     if thing.location.y < 0 {
-      thing.location.y = thing.location.y + 600
+      thing.location.y = thing.location.y + height
     }
 
     things[i] = thing
@@ -69,12 +69,6 @@ func update(elapsed float64) {
   }
 }
 
-func draw(elapsed float64) {
-  for i:= range things {
-    fmt.Printf("%d: (%f) %f, %f\n", i, things[i].size, things[i].location.x, things[i].location.y)
-  }
-}
-
 func(this Thing) intersects(other Thing) bool {
   distance := math.Sqrt(math.Pow((other.location.y - this.location.y), 2) + math.Pow((other.location.x - this.location.x),2) )
   return (this.size + other.size) >= distance
@@ -83,6 +77,10 @@ func(this Thing) intersects(other Thing) bool {
 func(this *Thing) absorb(other *Thing) {
   this.size += other.size
   other.size = 0
+}
+
+func random(min, max float64) float64 {
+  return rand.Float64() * (max - min) + min
 }
 
 type Vector struct {
@@ -102,19 +100,16 @@ func initGlfw(width, height int) {
     return
   }
 
-  if err = glfw.OpenWindow(width, height, 0, 0, 0, 0, 0, 0, glfw.Windowed); err != nil {
+  glfw.OpenWindowHint(glfw.FsaaSamples, 8)
+
+  if err = glfw.OpenWindow(width, height, 8, 8, 8, 8, 8, 8, glfw.Windowed); err != nil {
     fmt.Printf("%logv\n", err)
     return
   }
-
   glfw.SetWindowSizeCallback(onResize)
   glfw.SetKeyCallback(onKey)
   glfw.SetSwapInterval(1)
-  gl.Enable(gl.DEPTH_TEST)
   gl.Disable(gl.LIGHTING)
-  gl.ClearDepth(1)
-  gl.DepthFunc(gl.LEQUAL)
-  gl.Hint(gl.PERSPECTIVE_CORRECTION_HINT, gl.NICEST)
 }
 
 func terminateGlfw() {
@@ -125,35 +120,41 @@ func onResize(w, h int) {
   gl.MatrixMode(gl.PROJECTION)
   gl.LoadIdentity()
   gl.Viewport(0, 0, w, h)
-  //glu.Perspective(65.0, float64(w)/float64(h), 0.1, 2000.0)
-gl.Ortho(0, float64(w), float64(h), 0, -1, 1)
+  gl.Ortho(0, float64(w), float64(h), 0, -1, 1)
   gl.MatrixMode(gl.MODELVIEW)
   gl.LoadIdentity()
 }
 
 func onKey(key, state int) {
-  if glfw.Key(key) == glfw.KeyPress {
+  if key == glfw.KeyEsc && state == glfw.KeyPress {
     glfw.CloseWindow()
   }
 }
 
 func render() {
-  gl.ClearColor(0.0, 0.0, 0.05, 0)
+  const GL_MULTISAMPLE_ARB = 0x809D
+  gl.ClearColor(0.0, 0.0, 0.0, 0)
   gl.Clear(gl.COLOR_BUFFER_BIT)
+  gl.Enable(gl.BLEND)
+  gl.Enable(gl.LINE_SMOOTH)
+  gl.Enable(GL_MULTISAMPLE_ARB)
+  gl.Hint(gl.LINE_SMOOTH_HINT, gl.NICEST);
+  gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+  gl.LineWidth(1)
   gl.LoadIdentity()
-  gl.PointSize(1)
-  gl.Begin(gl.POINTS)
-    for t := range things {
-    x:=things[t].location.x
-    y:=things[t].location.y
-    radius := things[t].size
-    for i := 0; i < 100; i++ {
-        angle := float64(i)*2.0*math.Pi/100.0
-        gl.Color3ub(255,255,255)
-        gl.Vertex2f(float32(x + (math.Cos(angle) * radius)), float32(y + (math.Sin(angle) * radius)))
+  gl.Color3ub(255,255,255)
+  for t := range things {
+    if things[t].size == 0 {
+      continue
     }
-    }
-      //gl.Color3ub(uint8(k.x),uint8(k.y),uint8(k.z))//(rgb.r, rgb.g, rgb.b)
-      //gl.Vertex3f(float32(k.x), float32(k.y), float32(k.z))
-  gl.End()
+    gl.Begin(gl.LINE_LOOP)
+      x:=things[t].location.x
+      y:=things[t].location.y
+      radius := things[t].size
+      for i := 0.0; i < radius * 8; i++ {
+        angle := i*2.0*math.Pi/(radius * 8)
+        gl.Vertex2d(x + (math.Cos(angle) * radius), y + (math.Sin(angle) * radius))
+      }
+    gl.End()
+  }
 }
